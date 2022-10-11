3ded8cfa6c11bf9eb011ca40cd281c49b1610f0e
SpontanCombust <61706594+SpontanCombust@users.noreply.github.com>
2022-10-11 15:54:03 +0200

    more scenarios and favicon

--- src/DemoScenarios.ts
+++ src/DemoScenarios.ts
@@ -40,3 +40,65 @@ export function playDemoScenario3(table: PlayTable, canvas: HTMLCanvasElement) {
         table.balls[0].resetPushForce();
     }, 1000);
 }
+
+export function playDemoScenario4(table: PlayTable, canvas: HTMLCanvasElement) {
+    table.balls = [
+        new Ball(new Vec2(canvas.width / 2 - 300, canvas.height / 2), 50), 
+        new Ball(new Vec2(canvas.width / 2 + 200, canvas.height / 2), 50)
+    ];
+
+    table.balls[0].applyPushForce(new Vec2(15, 0));
+    table.balls[1].applyPushForce(new Vec2(-15, 0));
+
+    setTimeout(() => {
+        table.balls[0].resetPushForce();
+        table.balls[1].resetPushForce();
+    }, 1000);
+}
+
+export function playDemoScenario5(table: PlayTable, canvas: HTMLCanvasElement) {
+    table.balls = [
+        new Ball(new Vec2(canvas.width / 2 - 300, canvas.height / 2 - 200), 50), 
+        new Ball(new Vec2(canvas.width / 2 + 200, canvas.height / 2 + 200), 50)
+    ];
+
+    table.balls[0].applyPushForce(new Vec2(20, 20));
+    table.balls[1].applyPushForce(new Vec2(-10, -10));
+
+    setTimeout(() => {
+        table.balls[0].resetPushForce();
+        table.balls[1].resetPushForce();
+    }, 1000);
+}
+
+export function playDemoScenario6(table: PlayTable, canvas: HTMLCanvasElement) {
+    table.balls = [
+        new Ball(new Vec2(canvas.width / 2 - 300, canvas.height / 2 - 200), 50), 
+        new Ball(new Vec2(canvas.width / 2 + 100, canvas.height / 2 - 50), 50)
+    ];
+
+    table.balls[0].applyPushForce(new Vec2(20, 20));
+    table.balls[1].applyPushForce(new Vec2(-15, 15));
+
+    setTimeout(() => {
+        table.balls[0].resetPushForce();
+        table.balls[1].resetPushForce();
+    }, 1000);
+}
+
+export function playDemoScenario7(table: PlayTable, canvas: HTMLCanvasElement) {
+    table.balls = [
+        new Ball(new Vec2(canvas.width / 2 - 500, canvas.height / 2), 50), 
+        new Ball(new Vec2(canvas.width / 2 - 200, canvas.height / 2), 50), 
+        new Ball(new Vec2(canvas.width / 2 - 100, canvas.height / 2), 50), 
+        new Ball(new Vec2(canvas.width / 2 + 0, canvas.height / 2), 50),
+        new Ball(new Vec2(canvas.width / 2 + 100, canvas.height / 2), 50),
+        new Ball(new Vec2(canvas.width / 2 + 200, canvas.height / 2), 50),
+    ];
+
+    table.balls[0].applyPushForce(new Vec2(20, 0));
+
+    setTimeout(() => {
+        table.balls[0].resetPushForce();
+    }, 1000);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import Ball from "./Ball";
-import { playDemoScenario1, playDemoScenario2, playDemoScenario3 } from "./DemoScenarios";
+import { 
+    playDemoScenario1, 
+    playDemoScenario2, 
+    playDemoScenario3, 
+    playDemoScenario4, 
+    playDemoScenario5, 
+    playDemoScenario6, 
+    playDemoScenario7
+} from "./DemoScenarios";
 import PlayTable from "./PlayTable";
 import Vec2 from "./Vec2";
 
@@ -15,6 +23,10 @@ const DELTA_TIME_SEC = 1.0 / 60.0;
 document.getElementById("playScenario1Btn")?.addEventListener("click", () => playDemoScenario1(playTable, canvas));
 document.getElementById("playScenario2Btn")?.addEventListener("click", () => playDemoScenario2(playTable, canvas));
 document.getElementById("playScenario3Btn")?.addEventListener("click", () => playDemoScenario3(playTable, canvas));
+document.getElementById("playScenario4Btn")?.addEventListener("click", () => playDemoScenario4(playTable, canvas));
+document.getElementById("playScenario5Btn")?.addEventListener("click", () => playDemoScenario5(playTable, canvas));
+document.getElementById("playScenario6Btn")?.addEventListener("click", () => playDemoScenario6(playTable, canvas));
+document.getElementById("playScenario7Btn")?.addEventListener("click", () => playDemoScenario7(playTable, canvas));
 
 
 function gameLoop(): void {
